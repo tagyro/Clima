a2c3a5081ef24393501a1418fff8af2f9a0f28b7
tagyro <andrei.stoleru@gmail.com>
2015-11-21 14:21:27 +0100

    Custom implementation of GeoNames.org wrapper

---
+++ Clima/GeoNames/GeoNames.h
@@ -0,0 +1,13 @@
+//
+//  GeoNames.h
+//  Clima
+//
+//  Created by Andrei Stoleru on 21/11/15.
+//  Copyright © 2015 magooos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GeoNames : NSObject
+
+@end
---
+++ Clima/GeoNames/GeoNames.m
@@ -0,0 +1,13 @@
+//
+//  GeoNames.m
+//  Clima
+//
+//  Created by Andrei Stoleru on 21/11/15.
+//  Copyright © 2015 magooos. All rights reserved.
+//
+
+#import "GeoNames.h"
+
+@implementation GeoNames
+
+@end
