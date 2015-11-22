c34eca51d960a1422fc2063a6375f85d85fc751f
tagyro <andrei.stoleru@gmail.com>
2015-11-22 08:44:24 +0100

    Utility class

---
+++ Clima/Common.h
@@ -0,0 +1,14 @@
+//
+//  Common.h
+//  Clima
+//
+//  Created by Andrei Stoleru on 21/11/15.
+//  Copyright © 2015 magooos. All rights reserved.
+//
+
+#ifndef Common_h
+#define Common_h
+
+#define emptyImplementation(className)      @implementation className @end
+
+#endif /* Common_h */
