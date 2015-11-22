18b0e7eb2762b524c7e057db3d14c1698e9efaa9
tagyro <andrei.stoleru@gmail.com>
2015-11-22 08:44:46 +0100

    GeoNames.org wrapper for city names

--- Clima/GeoNames/GeoNames.h
+++ Clima/GeoNames/GeoNames.h
@@ -8,6 +8,41 @@
 
 #import <Foundation/Foundation.h>
 
+#import <JSONModel/JSONModel.h>
+
+#pragma mark - Models
+@protocol GNMLocation @end
+@interface GNMLocation : JSONModel
+@property (strong, nonatomic) NSString *toponymName;
+@property (strong, nonatomic) NSString *countryName;
+@property (strong, nonatomic) NSString *countryCode;
+@property (nonatomic) int postalCode;
+@end
+
+@interface GNMPostalCodes : JSONModel
+@property (strong, nonatomic) NSArray <GNMLocation> *geonames;
+@end
+
+
+#pragma mark - Events
+@interface GNEvent : JSONModel
+@property (strong, nonatomic) NSError *error;
+@end
+
+@interface GNEGetPlaces : GNEvent
+@property (strong, nonatomic) NSArray <GNMLocation> *locations;
+@end
+
+@interface GNESelectPlace : GNEvent
+@property (strong, nonatomic) GNMLocation *location;
+@end
+
+#pragma mark - Interface
+
 @interface GeoNames : NSObject
+
+- (instancetype)initWithUsername:(NSString*)username __attribute__((nonnull));
+
+- (void)findPlacesWithCharacters:(NSString*)characters __attribute__((nonnull));
 
 @end
--- Clima/GeoNames/GeoNames.m
+++ Clima/GeoNames/GeoNames.m
@@ -8,6 +8,84 @@
 
 #import "GeoNames.h"
 
+#import <AFNetworking/AFNetworking.h>
+
+#import <tolo/Tolo.h>
+
+#import "Common.h"
+
+@implementation GNMLocation
+
++ (BOOL)propertyIsOptional:(NSString *)propertyName {
+    return YES;
+}
+
+@end
+
+emptyImplementation(GNMPostalCodes)
+
+emptyImplementation(GNEvent)
+
+emptyImplementation(GNEGetPlaces)
+
+emptyImplementation(GNESelectPlace)
+
+static NSString *const kGeoNameURLString = @"http://api.geonames.org/";
+
+@interface GeoNames () {
+    AFHTTPRequestOperationManager *manager;
+    
+    NSString *uname;
+}
+
+@end
+
 @implementation GeoNames
+
+- (instancetype)initWithUsername:(NSString*)username {
+    self = [super init];
+    if (self) {
+        manager = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:[NSURL URLWithString:kGeoNameURLString]];
+        
+        uname = username;
+    }
+    return self;
+}
+
+- (void)findPlacesWithCharacters:(NSString*)characters {
+    if (isNull(characters)) {
+        return;
+    }
+    
+    NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
+                            uname, @"username",
+                            characters, @"name_startsWith",
+                            [NSNumber numberWithBool:true], @"isReduced",
+                            [NSNumber numberWithBool:true], @"isNameRequired",
+                            @"json",@"type",
+                            @"relevance",@"orderby",
+                            [NSNumber numberWithInt:10], @"maxRows", // limit to 10 results
+                            @"P",@"featureClass",
+                            nil];
+    
+    AFHTTPRequestOperation *getPlaces = [manager GET:@"search"
+                                          parameters:params
+                                             success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+                                                 NSError *error;
+                                                 GNMPostalCodes *locations = [[GNMPostalCodes alloc] initWithDictionary:responseObject error:&error];
+                                                 
+                                                 GNEGetPlaces *event = [GNEGetPlaces new];
+                                                 event.error = error;
+                                                 event.locations = locations.geonames;
+                                                 PUBLISH(event);
+                                                 
+                                             } failure:^(AFHTTPRequestOperation * _Nullable operation, NSError * _Nonnull error) {
+                                                 GNEGetPlaces *event = [GNEGetPlaces new];
+                                                 event.error = error;
+                                                 PUBLISH(event);
+                                             }];
+    
+    [getPlaces resume];
+}
 
 @end
