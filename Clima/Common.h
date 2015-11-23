286675edd26c48b239462e36b1ec48d57732de2d
tagyro <andrei.stoleru@gmail.com>
2015-11-23 01:12:28 +0100

    Follow the instruction to generate API keys

--- Clima/Common.h
+++ Clima/Common.h
@@ -11,4 +11,16 @@
 
 #define emptyImplementation(className)      @implementation className @end
 
+#error Register at https://www.flickr.com/services/api/misc.api_keys.html to generate API key
+static NSString *const kFlickKey        = @"";
+static NSString *const kFlickrSecret    = @"";
+
+#error Register at http://www.geonames.org/ to generate API key
+static NSString *const kGeoNameURLString = @"";
+
+#error Register at http://openweathermap.org/
+static NSString *const kOpenWeatherAPIKey = @"";
+
+static NSString *const kOpenWeatherURLString = @"http://api.openweathermap.org/data/2.5/";
+
 #endif /* Common_h */
